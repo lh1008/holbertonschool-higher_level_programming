e396d2489c2e4cea27330869fb06f5762e13efd0
lh1008 <paintluis@gmail.com>
2019-12-10 19:55:17 +0000

    00...99 again

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 for me in range(00, 100):
-    print('{:0>2d}{com}'.format(me, com = ", " if me is not 99 else "\n"), end='')
+    print('{:0>2d}{com}'.format(me, com=", "if me is not 99 else "\n"), end='')
