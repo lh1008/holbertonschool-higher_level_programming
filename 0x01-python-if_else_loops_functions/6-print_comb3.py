d29ab55f50a2edfeef30fd5b1b9680199fae111d
lh1008 <paintluis@gmail.com>
2019-12-10 20:58:45 +0000

    Printing

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for me in range(10):
     for run in range(10):
         if me < run:
-            print('{:d}{:d}'.format(me, run), end=', ')
-        if me == 8 and run == 9:
-            print('{:d}{:d}'.format(me, run))
+            print('{:d}{:d}'.format(me, run), end='')
+            if me < 8:
+                print(', ', end='')
