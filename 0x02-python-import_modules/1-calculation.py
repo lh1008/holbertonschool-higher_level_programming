3bad693cef520dbd9d26384fcce9304dd04cf09d
lh1008 <paintluis@gmail.com>
2019-12-12 21:06:26 +0000

    Missing import execution

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -6,7 +6,7 @@ if __name__ == '__main__':
     b = 5
 
 
-    print('{} + {} = {}'.format(a, b, calculator_1.add(a, b)))
-    print('{} - {} = {}'.format(a, b, calculator_1.sub(a, b)))
-    print('{} * {} = {}'.format(a, b, calculator_1.mul(a, b)))
-    print('{} / {} = {}'.format(a, b, calculator_1.div(a, b)))
+print('{} + {} = {}'.format(a, b, calculator_1.add(a, b)))
+print('{} - {} = {}'.format(a, b, calculator_1.sub(a, b)))
+print('{} * {} = {}'.format(a, b, calculator_1.mul(a, b)))
+print('{} / {} = {}'.format(a, b, calculator_1.div(a, b)))
