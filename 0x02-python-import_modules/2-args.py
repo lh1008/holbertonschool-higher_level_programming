1bf9c63beffa2ca02474d38a54ede1feb8f1aeba
lh1008 <paintluis@gmail.com>
2019-12-12 21:14:34 +0000

    Script dynamic, missing '.'

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,7 +2,7 @@
 if __name__ == '__main__':
     import sys
 if len(sys.argv) is 1:
-    print(0, 'arguments:')
+    print(0, 'arguments.')
 else:
     if len(sys.argv) is 2:
         print(len(sys.argv) - 1, 'argument:')
