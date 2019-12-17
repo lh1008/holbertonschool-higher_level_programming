dccd506c64f11d46a170faac23d716ad5f8ddf02
lh1008 <paintluis@gmail.com>
2019-12-17 20:22:45 +0000

    With i to iterate if print with space

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-
+    i = 0
     for r in matrix:
         for value in r:
-            print('{:d} '.format(value), end='')
+            if i == 0:
+                print('{:d}'.format(value), end='')
+            else:
+                print('{:2d}'.format(value), end='')
+            i = i + 1
+        i = 0
         print()
