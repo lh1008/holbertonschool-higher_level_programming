4b42ccedd517dcf6324711fe55d52c31b2d5ac1d
lh1008 <paintluis@gmail.com>
2020-01-20 17:45:01 -0500

    Improve Geometry

--- 0x0A-python-inheritance/5-base_geometry.py
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -5,4 +5,5 @@ Module for empyt class BaseGeometry
 
 
 class BaseGeometry:
+    """Class BaseGeometry"""
     pass
---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Module for empyt class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """Class BaseGeometry"""
+    def area(self):
+        """Method that raises an Exception"""
+        raise Exception("area() is not implemented")
