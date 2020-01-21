ad30232e3ad130fd4ca98ce30106131831f590de
lh1008 <paintluis@gmail.com>
2020-01-21 15:16:23 -0500

    Number of lines, Read n lines

---
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Module that returns the number of lines"""
+
+
+def number_of_lines(filename=""):
+    """Function that returns the number of line in a text file"""
+    with open('my_file_0.txt', 'r') as file_name:
+        return len(file_name.read().split('\n')) -1
---
+++ 0x0B-python-input_output/2-read_lines.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Module that reads lines oof a text file UTF8"""
+
+
+def read_lines(filename="", nb_lines=0):
+    """Method that read n lines of a text file UTF8"""
+    with open('my_file_0.txt', encoding='utf-8') as file_name:
+        nb_greater = len(file_name.read().split('\n')) -1
+        file_name.seek(0)
+        if nb_lines <= 0 or nb_lines > nb_greater:
+            print(file_name.read(), end='')
+        else:
+            for i in range(nb_lines):
+                print(file_name.readline(), end='')
