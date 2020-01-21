020ca76e23e319a97812a53e7faed2dfc648a755
lh1008 <paintluis@gmail.com>
2020-01-21 17:49:40 -0500

    Class to JSON

---
+++ 0x0B-python-input_output/10-class_to_json.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""
+Module that returns a dictionary description with simple
+data structure
+"""
+def class_to_json(obj):
+    """Method serialization of an object"""
+    return obj.__dict__
