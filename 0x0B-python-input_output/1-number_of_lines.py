88870dc7f81e613b3b4f5c12d531cbc5b81102ce
lh1008 <paintluis@gmail.com>
2020-01-21 21:31:00 -0500

    Fixing filename task 1

--- 0x0B-python-input_output/1-number_of_lines.py
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -4,5 +4,5 @@
 
 def number_of_lines(filename=""):
     """Function that returns the number of line in a text file"""
-    with open('my_file_0.txt', 'r') as file_name:
+    with open(filename, 'r') as file_name:
         return len(file_name.read().split('\n')) - 1
