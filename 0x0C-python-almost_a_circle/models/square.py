3ba6d0761f31dcb6419070dc06060f7ec66f5003
lh1008 <paintluis@gmail.com>
2020-01-27 12:45:38 -0500

    Square size

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -10,7 +10,20 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         """Class constructor method for square"""
         super().__init__(size, size, x, y, id)
+        self.size = size
 
     def __str__(self):
         """Method overloading a square"""
         return '[Square] ({self.id}) {self.x}/{self.y} - {self.width}'.format(self=self)
+
+    @property
+    def size(self):
+        """Sets the value of width"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Sets the value width"""
+        self.width = value
+        self.height = value
+        self.__size = value
