b76227061060937ddf42375b80632ae76328b9de
lh1008 <paintluis@gmail.com>
2020-03-02 23:18:53 -0500

    16. Say my name, going back

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,2 +1,2 @@
 -- Script that lists all records of the table `second_table` of the database `hbtn_0c_0`
-SELECT score, name FROM second_table WHERE score>=10 GROUP BY score DESC, name;
+SELECT score, name FROM second_table GROUP BY score DESC, name;
