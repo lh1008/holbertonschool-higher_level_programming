4d51918b1991b10697e3e0b6b25a0270a9f9e96f
lh008 <paintluis@gmail.com>
2020-03-09 16:54:31 -0500

    Foreign Key(state_id)

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -5,8 +5,7 @@ CREATE TABLE IF NOT EXISTS cities (
        id INT UNIQUE AUTO_INCREMENT NOT NULL,
        state_id INT NOT NULL,
        PRIMARY KEY (id),
-       FOREIGN KEY (id)
+       FOREIGN KEY(state_id)
        	       REFERENCES states(id),
        name VARCHAR(256) NOT NULL
-       )
-       type=InnoDB;
+       );
