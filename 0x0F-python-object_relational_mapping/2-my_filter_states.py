767f0cf083e726a0bc5404fbde3e63fdae2d2969
lh008 <paintluis@gmail.com>
2020-03-29 18:43:23 -0500

    Removed spaces in connect

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -7,11 +7,11 @@ import sys
 def main():
     """ Main method that catches the arguments """
     name = sys.argv[4]
-    db = MySQLdb.connect(host = "localhost",
-                         port = 3306,
-                         user = sys.argv[1],
-                         passwd = sys.argv[2],
-                         db = sys.argv[3])
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3])
 
     cur = db.cursor()
     cur.execute("""SELECT * FROM states
