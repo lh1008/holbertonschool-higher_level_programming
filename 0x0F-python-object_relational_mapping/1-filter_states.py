8bc28a042b1f55bf4afa5214b702a39de12b178a
lh008 <paintluis@gmail.com>
2020-03-29 18:17:43 -0500

    Adding Binary  in the query, task 1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -13,7 +13,7 @@ def main():
                          db=sys.argv[3])
 
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC")
     for grab in cur.fetchall():
         print(grab)
 
