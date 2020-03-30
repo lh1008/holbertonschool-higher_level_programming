d8cc3434b7e7b2847c225538c9f54e85d5ede30b
lh008 <paintluis@gmail.com>
2020-03-29 19:18:42 -0500

    SQL injection task 3

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" Module that lists all states starting with N from database """
+import MySQLdb
+import sys
+
+
+def main():
+    """ Main method that catches the arguments """
+    name = sys.argv[4]
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3])
+
+    cur = db.cursor()
+    cur.execute("""SELECT *
+    FROM states
+    WHERE name LIKE BINARY %(name)s
+    ORDER BY id ASC""", {'name': name})
+    for grab in cur.fetchall():
+        print(grab)
+
+if __name__ == '__main__':
+    main()
