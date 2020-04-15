c358fe193245395bd983782996705313b63e173a
lh008 <paintluis@gmail.com>
2020-04-15 14:52:43 -0500

    POST an email #0, Task 2

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" Module that sends a POST request to a passed URL """
+import urllib.parse
+import urllib.request
+from sys import argv
+
+
+url = argv[1]
+values = { 'email' : argv[2] }
+data = urllib.parse.urlencode(values)
+data = data.encode('ascii')
+req = urllib.request.Request(url, data)
+with urllib.request.urlopen(req) as response:
+    html = response.read()
+    print(html.decode())
