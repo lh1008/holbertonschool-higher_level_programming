2ca4c282f0410eebe9ab5ab17c58e997dde0e390
lh008 <paintluis@gmail.com>
2020-04-15 16:54:47 -0500

    POST an email #1, Task 6

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" Module that takes in a URL and sends a POST """
+import requests
+from sys import argv
+
+
+req = requests.post(argv[1], data = {'email' : argv[2]})
+print(req.content.decode())
