27772de74db1c29f25f0f5d3266a04af67e073cb
lh008 <paintluis@gmail.com>
2020-04-16 15:54:10 -0500

    Search API, Task 8

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" Module that sends a POST request with a letter as parameter """
+import requests
+from sys import argv
+
+
+if len(argv) == 1 or argv[1].isdigit():
+    print('No result')
+else:
+    req = requests.post('http://0.0.0.0:5000/search_user', data={'q': argv[1]})
+    k = req.json()
+    print('[{}] {}'.format(k.get('id'), k.get('name')))
