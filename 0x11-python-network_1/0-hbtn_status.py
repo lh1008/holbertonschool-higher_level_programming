6e8f1e8797c3dba7c9320c82ae0905a405f9251d
lh008 <paintluis@gmail.com>
2020-04-16 20:02:56 -0500

    Task 0 missing 'Body response:'

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,6 +7,7 @@ def main():
     """ Method that fetches a website """
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         html = response.read()
+        print('Body response:')
         print('\t- type: {}'.format(type(html)))
         print('\t- content: {}'.format(html))
         print('\t- utf8 content: {}'.format(html.decode()))
