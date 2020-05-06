c9ce90298d56d4c8d2981c39b663ee023d5af6db
lh008 <paintluis@gmail.com>
2020-05-05 20:25:21 -0500

    Missing 'My number:' Task 5

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,7 +2,7 @@
 if (process.argv.length < 3) {
   console.log('Not a number');
 } else if (isNaN(process.argv[2]) === false) {
-  console.log(parseInt(process.argv[2]));
+  console.log('My number: ' + parseInt(process.argv[2]));
 } else if (typeof process.argv[2] === 'string') {
   console.log('Not a number');
 }
