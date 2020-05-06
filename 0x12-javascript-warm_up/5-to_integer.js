ebb509a0ecfcb7fc5245b2d40371864aa0090185
lh008 <paintluis@gmail.com>
2020-05-05 20:22:19 -0500

    To integer, Task 5, isNaN === false

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 if (process.argv.length < 3) {
-  console.log('Not a number')
-} else if (parseInt(process.argv[2])) {
+  console.log('Not a number');
+} else if (isNaN(process.argv[2]) === false) {
   console.log(parseInt(process.argv[2]));
 } else if (typeof process.argv[2] === 'string') {
   console.log('Not a number');
