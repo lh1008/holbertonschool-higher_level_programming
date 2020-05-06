8d9a5ca1c6b0555d9a87a0396f95875b79724514
lh008 <paintluis@gmail.com>
2020-05-05 19:10:42 -0500

    Task 4, Correction of sentences in else

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -2,5 +2,5 @@
 if (process.argv.length > 2) {
   console.log(process.argv[2] + ' is ' + process.argv[3]);
 } else {
-  console.log('undifined' + ' is ' + process.argv[3]);
+  console.log(process.argv[2] + ' is ' + process.argv[3]);
 }
