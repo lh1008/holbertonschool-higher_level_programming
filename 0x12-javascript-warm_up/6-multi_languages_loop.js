41561c9e5908b0f4f070f84cb558b9459ea829b8
lh008 <paintluis@gmail.com>
2020-05-05 21:10:24 -0500

    Semistandard missing requirements, Task 6

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-let myVar = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-myVar.forEach(function(item, array) {
+const myVar = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+myVar.forEach(function (item, array) {
   console.log(item);
-})
+});
