a0267b5eb5e5304290bf5d8be7caae00018b476d
lh008 <paintluis@gmail.com>
2020-05-06 22:56:55 -0500

    Task 8, unfinished, can't make print Missing size when string

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,8 +1,13 @@
 #!/usr/bin/node
 if (process.argv.length < 3) {
   console.log('Missing size');
-} else {
-  for (let count = 0; count < parseInt(process.argv[2]); count++) {
-    console.log('X'.repeat(parseInt(process.argv[2])));
+} else if (typeof process.argv[2] === 'string') {
+  if (isNaN(parseInt(process.argv[2]) === false)) {
+    console.log('Missing size')
+  } else {
+    for (let count = 0; count < parseInt(process.argv[2]); count++) {
+      console.log('X'.repeat(parseInt(process.argv[2])));
+    }
   }
 }
+  console.log(isNaN(process.argv[2]));
