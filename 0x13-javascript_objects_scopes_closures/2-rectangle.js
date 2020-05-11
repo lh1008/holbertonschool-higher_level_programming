4e119ace723e3a28b63bfc4fae9c9a1403137fd1
lh008 <paintluis@gmail.com>
2020-05-11 18:49:42 -0500

    semistandard missing, Task 2, else statement not correctly placed

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,13 +1,11 @@
 #!/usr/bin/node
 module.exports = class Rectangle {
   constructor (w, h) {
-    if (w < 0 || h < 0 || w === 0 || h === 0 ||
-	typeof w === 'undefined' || typeof h === 'undefined') {
+    if (w < 0 || h < 0 || w === 0 || h === 0 || typeof w === 'undefined' || typeof h === 'undefined') {
       module.exports = class Rectangle {};
-    }
-    else {
-    this.width = w;
-    this.height = h;
+    } else {
+      this.width = w;
+      this.height = h;
     }
   }
 };
