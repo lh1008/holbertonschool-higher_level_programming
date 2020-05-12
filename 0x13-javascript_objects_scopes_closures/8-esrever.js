d3dfaf2dede5a9b9f214afc717cafb94c4d20638
lh008 <paintluis@gmail.com>
2020-05-11 21:20:53 -0500

    semistandard fix camelCase, remove '_'

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
   const line = list;
-  const line_2 = [];
+  const line2 = [];
 
   for (let i = line.length - 1; i >= 0; i--) {
-    line_2.push(line[i]);
+    line2.push(line[i]);
   }
-  return line_2;
+  return line2;
 };
