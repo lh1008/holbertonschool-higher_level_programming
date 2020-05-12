c8c46aba56cfb498f92c8a30c4e8d1b5058345f6
lh008 <paintluis@gmail.com>
2020-05-12 16:55:09 -0500

    Status code, Task 2

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const request = require('request');
+
+const options = {
+  url: process.argv[2],
+  method: 'GET',
+};
+request(options, function(err, res, body) {
+  console.log('code: ' + res.statusCode);
+});
