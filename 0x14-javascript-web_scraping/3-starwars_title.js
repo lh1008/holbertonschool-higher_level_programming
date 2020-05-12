0a8672c3d432cc09aa828dd92d0c94415f486472
lh008 <paintluis@gmail.com>
2020-05-12 18:31:23 -0500

    semistandard for Task 3

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const request = require('request');
 
-let  url = 'https://swapi-api.hbtn.io/api/films/'
+const url = 'https://swapi-api.hbtn.io/api/films/';
 
 request.get(url.concat(process.argv[2]), function (err, res, body) {
   if (err) {
