77cd22fe769c693c6193ff961342c97701dacff1
lh008 <paintluis@gmail.com>
2020-05-11 20:07:27 -0500

    semistandard requirments missing, Task 4

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -15,14 +15,14 @@ module.exports = class Rectangle {
     }
   }
 
-  rotate() {
+  rotate () {
     const temp = this.height;
     this.height = this.width;
-    this.width = temp
+    this.width = temp;
   }
 
   double () {
-    this.width = this.width * 2
-    this.height = this.height * 2
+    this.width = this.width * 2;
+    this.height = this.height * 2;
   }
 };
