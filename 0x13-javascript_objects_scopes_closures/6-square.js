b58da60208720209b8f109467e33247265c4eed4
lh008 <paintluis@gmail.com>
2020-05-11 21:00:31 -0500

    Square #1, Task 6

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+class Square extends require('./5-square') {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    if (typeof c === 'undefined') {
+      this.print();
+    } else {
+      for (let move = 0; move < this.height; move++) {
+        console.log(c.repeat(this.width));
+      }
+    }
+  }
+};
+
+
+module.exports = Square;
