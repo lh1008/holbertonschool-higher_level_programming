a206ebe16074d4939bef06e87ae96169781ca770
lh008 <paintluis@gmail.com>
2020-05-11 20:05:37 -0500

    Rectangle #4, Task 4

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,28 @@
+#!/usr/bin/node
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w < 0 || h < 0 || w === 0 || h === 0 || typeof w === 'undefined' || typeof h === 'undefined') {
+      module.exports = class Rectangle {};
+    } else {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let move = 0; move < this.height; move++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+
+  rotate() {
+    const temp = this.height;
+    this.height = this.width;
+    this.width = temp
+  }
+
+  double () {
+    this.width = this.width * 2
+    this.height = this.height * 2
+  }
+};
