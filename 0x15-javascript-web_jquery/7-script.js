896742a2a7278069281b5c6d3e6b38b8ebfe697d
lh008 <paintluis@gmail.com>
2020-05-14 21:28:10 -0500

    Star wars character, Task 7

---
+++ 0x15-javascript-web_jquery/7-script.js
@@ -0,0 +1,6 @@
+const url = 'https://swapi-api.hbtn.io/api/people/5/?format=json'
+$.get(url , function(data, textStatus) {
+  if (textStatus == 'success') {
+    $('DIV#character').text(data.name);
+  }
+});
